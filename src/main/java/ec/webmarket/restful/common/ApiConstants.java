66e322746ad5502431019f52a0a5a3e96e5348af
hierbamalax <apcepedam@pucesm.edu.ec>
2025-02-16 12:14:48 -0500

    Final

--- src/main/java/ec/webmarket/restful/common/ApiConstants.java
+++ src/main/java/ec/webmarket/restful/common/ApiConstants.java
@@ -2,11 +2,13 @@ package ec.webmarket.restful.common;
 
 public final class ApiConstants {
 
-	private ApiConstants() {
-	}
+    private ApiConstants() {
+    }
 
-	public static final String URI_API_V1 = "/api/v1.0/";
+    public static final String URI_API_V1 = "/api/v1.0/";
 
-	public static final String URI_API_V1_PAIS = URI_API_V1 + "pais";
-	public static final String URI_API_V1_PROVINCIA = URI_API_V1 + "provincia";
+    public static final String URI_API_V1_PACIENTE = URI_API_V1 + "paciente";
+    public static final String URI_API_V1_ODONTOLOGO = URI_API_V1 + "odontologo";
+    public static final String URI_API_V1_HORARIO = URI_API_V1 + "horario";
+    public static final String URI_API_V1_CITA = URI_API_V1 + "cita";
 }
